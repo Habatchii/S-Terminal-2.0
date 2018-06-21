3f1e6c91e137cad0dba5231a4f9d8a936fcdba33
Stacy Staples <habatchii@aol.com>
2018-06-21 15:36:31 -0500

    Update_072118

---
+++ S-Terminal 2.0/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_Terminal_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
